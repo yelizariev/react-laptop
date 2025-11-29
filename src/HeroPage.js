f5b6a90cc18ca532dfb5ee131c82e9967543fe42
Ivan Yelizariev <info@odoomagic.com>
2025-11-29 12:21:52 +0100

    fix-fix

--- src/HeroPage.js
+++ src/HeroPage.js
@@ -313,40 +313,12 @@ export default function Example() {
       </Popover>
 
       <main>
-        <div className="mx-auto max-w-7xl w-full pt-16 pb-20 text-center">
-          <div className="px-4 px-8">
-            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 text-5xl">
-              <span className="block">Data to enrich your</span> <span className="block text-indigo-600">online business</span>
-            </h1>
-            <p className="mt-3 max-w-md mx-auto text-lg text-gray-500 text-xl">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
-              occaecat fugiat aliqua.
-            </p>
-            <div className="mt-10 flex justify-center">
-              <div className="mt-3 rounded-md shadow mt-0 ml-3">
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
-                  Get started
-                </a>
-              </div>
-              <div className="mt-3 rounded-md shadow mt-0 ml-3">
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50">
-                  Live demo
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="relative w-full h-64 h-72">
-          <img
-            className="absolute inset-0 w-full h-full object-cover"
-            src="https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80"
-            alt=""
+          <div
+      className="mx-auto max-w-7xl w-full pt-16 pb-20 px-4 text-left"
+      dangerouslySetInnerHTML={{
+          __html: window.APP_CONFIG?.htmlContent || '',
+      }}
           />
-        </div>
       </main>
     </div>
   )
